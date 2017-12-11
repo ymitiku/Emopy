3c8090068c8a9431b7287e95d124d6240ca93df8
Mitiku <se.mitiku.yohannes@gmail.com>
2017-12-11 13:19:39 +0300

    added kernel initializers for convnets

--- nets/multinput.py
+++ nets/multinput.py
@@ -61,10 +61,10 @@ class MultiInputNeuralNet(NeuralNet):
         image_layer = Conv2D(32, (7, 7), activation='relu',padding= "valid",kernel_initializer="glorot_normal")(image_input_layer)
         image_layer = Dropout(.2)(image_layer)
         image_layer = MaxPooling2D(pool_size=(2, 2))(image_layer)
-        image_layer = Conv2D(64,(7,7),activation = "relu",padding="valid")(image_layer)
+        image_layer = Conv2D(64,(5,5),activation = "relu",padding="valid",kernel_initializer="glorot_normal")(image_layer)
         image_layer = Dropout(.2)(image_layer)
         image_layer = MaxPooling2D(pool_size=(2, 2))(image_layer)
-        image_layer = Conv2D(128,(7,7),activation = "relu",padding="valid")(image_layer)
+        image_layer = Conv2D(128,(3,3),activation = "relu",padding="valid",kernel_initializer="glorot_normal")(image_layer)
         image_layer = Dropout(.2)(image_layer)
 
         image_layer = Flatten()(image_layer)
@@ -72,21 +72,21 @@ class MultiInputNeuralNet(NeuralNet):
         dlib_points_input_layer = Input(shape=(1,68,2))
         dlib_points_layer = Conv2D(32, (1, 3), activation='relu',padding= "valid",kernel_initializer="glorot_normal")(dlib_points_input_layer)
         dlib_points_layer = MaxPooling2D(pool_size=(1, 2))(dlib_points_layer)
-        dlib_points_layer = Conv2D(64,(1, 3),activation = "relu",padding="valid")(dlib_points_layer)
+        dlib_points_layer = Conv2D(64,(1, 3),activation = "relu",padding="valid",kernel_initializer="glorot_normal")(dlib_points_layer)
 
         dlib_points_layer = Flatten()(dlib_points_layer)
 
         dlib_points_dist_input_layer = Input(shape=(1,68,1))
         dlib_points_dist_layer = Conv2D(32, (1, 3), activation='relu',padding= "valid",kernel_initializer="glorot_normal")(dlib_points_dist_input_layer)
         dlib_points_dist_layer = MaxPooling2D(pool_size=(1, 2))(dlib_points_dist_layer)
-        dlib_points_dist_layer = Conv2D(64,(1, 3),activation = "relu",padding="valid")(dlib_points_dist_layer)
+        dlib_points_dist_layer = Conv2D(64,(1, 3),activation = "relu",padding="valid",kernel_initializer='glorot_normal')(dlib_points_dist_layer)
 
         dlib_points_dist_layer = Flatten()(dlib_points_dist_layer)
 
         dlib_points_angle_input_layer = Input(shape=(1,68,1))
         dlib_points_angle_layer = Conv2D(32, (1, 3), activation='relu',padding= "valid",kernel_initializer="glorot_normal")(dlib_points_angle_input_layer)
         dlib_points_angle_layer = MaxPooling2D(pool_size=(1, 2))(dlib_points_angle_layer)
-        dlib_points_angle_layer = Conv2D(64,(1, 3),activation = "relu",padding="valid")(dlib_points_angle_layer)
+        dlib_points_angle_layer = Conv2D(64,(1, 3),activation = "relu",padding="valid",kernel_initializer='glorot_normal')(dlib_points_angle_layer)
 
         dlib_points_angle_layer = Flatten()(dlib_points_angle_layer)
 
