921189444d6d9cf270913a6709377aae5bf40506
Mitiku <se.mitiku.yohannes@gmail.com>
2017-12-16 11:16:50 +0300

    simple model for rnn

--- nets/rnn.py
+++ nets/rnn.py
@@ -27,72 +27,18 @@ class LSTMNet(NeuralNet):
         self.model = self.build()
         self.models_local_folder = "rnn"
     def build(self):
-        # convnet_model = self.load_model()
-        # for layer in convnet_model.layers:
-        #     layer.trainable = False
-
-        # convnet_model.layers.pop()
-        # convnet_model.layers.pop()
-        # print "conv net",convnet_model.summary()
-        # convnet_model.summary()
-
-        # input_layer = Input()
-
-        # # # conv_input = Reshape((self.input_shape))(input_layer)
-        # # # conv_input = K.reshape(input_layer,(-1,self.input_shape[0],self.input_shape[1],self.input_shape[2]))
-        # conv_input = Lambda(lambda x: K.reshape(x,(-1,self.input_shape[0],self.input_shape[1],self.input_shape[2])))(input_layer)
-
-        # convnet_model = convnet_model(conv_input)
-        # # last_layer = convnet_model.layers[12].output
-        # # last_layer_shape = last_layer.shape
-
-        # # # last_layer = K.reshape(last_layer,(-1,last_layer_shape[1],self.max_sequence_length,1))
-        # convnet_model = Lambda(lambda x: K.reshape(x,(-1,self.max_sequence_length,1)))(convnet_model)
-        
-        
-        # lstm_input_layer = Reshape((self.max_sequence_length,self.input_shape[0]*self.input_shape[1]*self.input_shape[2]))(input_layer)
-        # # print self.input_shape[0]*self.input_shape[1]*self.input_shape[2]
-        # lstm_model = LSTM(64,input_shape=(self.max_sequence_length,(self.input_shape[0]*self.input_shape[1]*self.input_shape[2])), return_sequences=False)
-        # lstm_model = lstm_model(lstm_input_layer)
-        # print "lstm _model",lstm_model.shape
-        # lstm_model = Lambda(lambda x: K.reshape(x,(-1,self.max_sequence_length,1)))(lstm_model)
-        # print "convNet",convnet_model.shape
-        # print "lstm",lstm_model.shape
-        # help(merge)
-        # x = merge([convnet_model,lstm_model],mode="dot",dot_axes=-2)
-        # x = multiply([convnet_model,lstm_model])
-
-        # predictions = Dense(self.preprocessor.classifier.get_num_class(),activation="softmax")(lstm_model)
-
-        # model = Model(inputs=input_layer,outputs=predictions)
-
-        # model = Sequential()
-        # model.add(Conv2D(32,(5,5),input_shape=(self.input_shape[0],self.input_shape[1],self.input_shape[2]),padding="valid",strides=1))
-        # conv_input_shape = self.max_sequence_length, self.input_shape[0],self.input_shape[1],self.input_shape[2]
-        # model.add(TimeDistributed(Conv2D(64, (3, 3)),
-                        #   input_shape=(conv_input_shape)))
-        # model.add(Bidirectional(LSTM(64, return_sequences=True)))
-        # model.add(Bidirectional(LSTM(128,return_sequences=False)))
-        # model.add(TimeDistributed(Dense(6)))
-        # model.add(Activation('softmax'))
 
         model = Sequential()
 
         model.add(TimeDistributed(Conv2D(32, (3, 3), padding='valid', activation='relu'),input_shape=(self.max_sequence_length, 48, 48, 1)))
-        # model.add(TimeDistributed(Conv2D(64, (3, 3), padding='valid', activation='relu')))
-        # model.add(TimeDistributed(MaxPooling2D(pool_size=(2,2))))
-        model.add(TimeDistributed(Dropout(0.2)))
-        model.add(TimeDistributed(Conv2D(128, (3, 3), padding='valid', activation='relu')))
-        model.add(TimeDistributed(Dropout(0.2)))
+        # model.add(TimeDistributed(Conv2D(64,(3,3),padding="valid",activation="relu")))
+        # model.add(TimeDistributed(Dropout(0.2)))
         model.add(TimeDistributed(MaxPooling2D(pool_size=(2,2))))
         model.add(TimeDistributed(Flatten()))
-        model.add(TimeDistributed(Dense(252,activation="relu")))
-        model.add(TimeDistributed(Dropout(0.2)))
-        # model.add(LSTM(64,return_sequences=True,stateful=False))
-        model.add(LSTM(64,return_sequences=False,stateful=False))
-        model.add(Dropout(0.2))
-        model.add(Dense(128,activation="softmax"))
-        model.add(Dropout(0.2))
+        model.add(Bidirectional(LSTM(128,return_sequences=False,stateful=False,activation="relu",recurrent_dropout=0.2)))
+        # model.add(Dropout(0.2))
+        # model.add(Dense(128,activation="relu"))
+        # model.add(Dropout(0.2))
         model.add(Dense(6,activation="softmax"))
         
         return model;
@@ -104,6 +50,11 @@ class LSTMNet(NeuralNet):
             model = model_from_json(model_file.read())
             model.load_weights(self.convnet_model_path+".h5")
             return model
+    def predict(self,sequence_faces):
+        assert sequence_faces[0].shape == IMG_SIZE, "Face image size should be "+str(IMG_SIZE)
+        face = face.reshape(-1,self.max_sequence_length,48,48,1)
+        emotions = self.model.predict(face)[0]
+        return emotions
     def train(self):
         """Traines the neuralnet model.      
         This method requires the following two directory to exist
@@ -128,7 +79,8 @@ class LSTMNet(NeuralNet):
                         epochs=EPOCHS,
                         validation_data=(self.preprocessor.test_sequences, self.preprocessor.test_sequence_labels))
         
-        score = self.model.evaluate(self.preprocessor.test_images, self.preprocessor.test_image_emotions)
+        score = self.model.evaluate(self.preprocessor.test_sequences, self.preprocessor.test_sequence_labels)
         self.save_model()
         self.logger.log_model(self.models_local_folder, score)
+
     
