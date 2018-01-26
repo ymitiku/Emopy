77133a1123c2f2bc335ff942fe96880b1f696d31
Mitiku <se.mitiku.yohannes@gmail.com>
2018-01-26 14:20:04 +0300

    changed batch sise to 100

--- train_config.py
+++ train_config.py
@@ -1,9 +1,9 @@
-BATCH_SIZE = 32 # Batch sized used for traing.
+BATCH_SIZE = 128 # Batch sized used for traing.
 EPOCHS = 100
 LEARNING_RATE = 1e-4
 PATH2SAVE_MODELS = "models"
 DATA_SET_DIR = "/home/mtk/iCog/projects/emopy/dataset/all"
 LOG_DIR = "logs"
-STEPS_PER_EPOCH = 1000
+STEPS_PER_EPOCH = 500
 NETWORK_TYPE = "mi" # mi for multi input or si for single input
 AUGMENTATION = True
